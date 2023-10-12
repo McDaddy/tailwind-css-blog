8c108286d680efc14328c8a9d24d2be5671a8793
McDaddy <mcdaddychen@126.com>
2023-10-12 19:30:06 +0800

    feat: custom info

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,18 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'McDaddy的Blog',
+  author: 'McDaddy',
+  headerTitle: 'McDaddy的Blog',
+  description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
+  siteLogo: '/static/images/avatar.jpg',
+  socialBanner: '/static/images/avatar.jpg',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'mcdaddychen@126.com',
+  github: 'https://github.com/McDaddy',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
